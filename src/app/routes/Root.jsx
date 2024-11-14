907e548c6788024104111cfec50f4cead0a8fc17
arthur <artyr007@gmail.com>
2024-11-14 20:28:45 +0500

    feat(dev footer): add DevFooter component and related css styles

--- src/app/routes/Root.jsx
+++ src/app/routes/Root.jsx
@@ -4,6 +4,7 @@ import Banner from "@components/Banner/Banner.jsx";
 import FeaturedProducts from "@features/FeaturedProducts/FeaturedProducts.jsx";
 import Sustainability from "@components/Sustainability/Sustainability.jsx";
 import Footer from "@components/Footer/Footer.jsx";
+import DevFooter from "@components/DevFooter/DevFooter.jsx";
 
 export default function Root() {
   return (
@@ -14,6 +15,7 @@ export default function Root() {
       <FeaturedProducts />
       <Sustainability />
       <Footer />
+      <DevFooter />
     </>
   );
 }
---
+++ src/components/DevFooter/DevFooter.jsx
@@ -0,0 +1,16 @@
+import ghIcon from "@assets/svg/gh-icon.svg";
+import styles from "./DevFooter.module.css";
+
+export default function DevFooter() {
+  return (
+    <section className={styles.dev}>
+      <div className={styles["dev-author"]}>
+        <a href="https://github.com/arthwr" target="_blank" rel="noopener noreferrer">
+          <img src={ghIcon} alt="github profile" />
+        </a>
+        <span>Designed & Developed by Arthur Utegenov</span>
+      </div>
+      <div className={styles.copyright}>&copy; Copyright {new Date().getFullYear()}. All rights reserved</div>
+    </section>
+  );
+}
