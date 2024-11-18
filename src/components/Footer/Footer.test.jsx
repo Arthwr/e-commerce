d86fc8753167541245e41603225aad796d18019e
arthur <artyr007@gmail.com>
2024-11-18 23:37:53 +0500

    tests(Footer): add related tests

---
+++ src/components/Footer/Footer.test.jsx
@@ -0,0 +1,37 @@
+import { render, screen } from "@testing-library/react";
+import { MemoryRouter } from "react-router-dom";
+import { describe, expect, it } from "vitest";
+import Footer from "./Footer";
+
+describe("Footer component", () => {
+  it("renders all internal links correctly", () => {
+    render(
+      <MemoryRouter>
+        <Footer />
+      </MemoryRouter>
+    );
+
+    const links = screen.getAllByRole("link", { name: /Help Center|Community|FAQ|Contact Us/ });
+    expect(links).toHaveLength(4);
+
+    links.forEach((link) => {
+      expect(link).toHaveAttribute("href", "/about");
+    });
+  });
+
+  it("renders all external links correctly", () => {
+    render(
+      <MemoryRouter>
+        <Footer />
+      </MemoryRouter>
+    );
+
+    const twitterLink = screen.getByText("Twitter").closest("a");
+    const instagramLink = screen.getByText("Instagram").closest("a");
+    const faceboookLink = screen.getByText("Facebook").closest("a");
+
+    expect(twitterLink).toHaveAttribute("href", "https://twitter.com");
+    expect(instagramLink).toHaveAttribute("href", "https://instagram.com");
+    expect(faceboookLink).toHaveAttribute("href", "https://facebook.com");
+  });
+});
