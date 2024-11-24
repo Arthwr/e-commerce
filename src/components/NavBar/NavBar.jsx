c79b3a16956333bfc7758d77fe5a61e7eacc6a32
arthur <artyr007@gmail.com>
2024-11-24 22:42:08 +0500

    feat(NavBar): add fixed position to navbar and hiding/showing functionality effect on scroll

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -4,10 +4,28 @@ import logo from "@assets/svg/main-logo.svg";
 import searchIcon from "@assets/svg/search-icon.svg";
 import cartIcon from "@assets/svg/cart-icon.svg";
 import styles from "./NavBar.module.css";
+import { useEffect, useState } from "react";
 
 export default function NavBar({ isAnimating = false }) {
+  const [prevScrollPos, setPrevScrollPos] = useState(0);
+  const [visible, setVisible] = useState(true);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      const currentScrollPos = window.scrollY;
+
+      setVisible(prevScrollPos > currentScrollPos || currentScrollPos < 50);
+
+      setPrevScrollPos(currentScrollPos);
+    };
+
+    window.addEventListener("scroll", handleScroll);
+
+    return () => window.removeEventListener("scroll", handleScroll);
+  }, [prevScrollPos]);
+
   return (
-    <header>
+    <header className={`${visible ? "" : styles.hidden}`}>
       <nav>
         <div className={styles.left}>
           <div className={`${styles.logo} ${isAnimating ? styles.visible : ""}`}>
