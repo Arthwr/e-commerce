7da4b3a31f73063d9d18982cde676f98e8eef733
arthur <artyr007@gmail.com>
2024-11-30 22:19:10 +0500

    feat(cart): add checkout actions and related styles

--- src/features/Cart/Cart.jsx
+++ src/features/Cart/Cart.jsx
@@ -1,6 +1,8 @@
 import CartItem from "@components/CartItem/CartItem";
 import styles from "./Cart.module.css";
 import img from "@assets/images/cart-topbg.webp";
+import ActionButton from "@components/ActionButton/ActionButton";
+import { Link } from "react-router-dom";
 
 export default function Cart() {
   return (
@@ -21,7 +23,18 @@ export default function Cart() {
           <CartItem name="Product B" price={2000} quantity={4} total={8000} />
           <CartItem name="Product C" price={349} quantity={1} total={349} />
         </div>
-        <div className={styles.actions}></div>
+        <div className={styles["checkout-group"]}>
+          <div className={styles["total-wrapper"]}>
+            <div className={styles["total-label"]}>Subtotal:</div>
+            <span className={styles["total-price"]}>8549 $</span>
+          </div>
+          <div className={styles.actions}>
+            <ActionButton path="/checkout" label="Checkout" ariaLabel="confirm purchase" />
+            <Link to="/" className={styles.return}>
+              Keep shopping
+            </Link>
+          </div>
+        </div>
       </div>
     </section>
   );
